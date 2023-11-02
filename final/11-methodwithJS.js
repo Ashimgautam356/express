25d0cb487f89ab94a41a78a9e58a7be62333713a
Ashim Gautam <computer77356@gmail.com>
2023-11-02 22:18:09 +0545

    learned about postman

--- final/11-methodwithJS.js
+++ final/11-methodwithJS.js
@@ -10,21 +10,14 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.get('/api/people',(req,res)=>{
-    res.status(200).json({success:true, data:people})
+    res.status(200).json({sucess:true, data:people})
 })
 
-app.post('/api/postman/people',(req, res)=>{
-    const{name} = req.body
-    if(!name){
-        return res.status(400).json({success:false, msg:'please provid name value'})
-    }
-    res.status(201).send({success:true, data:[...people, name]})
-})
 
 app.post('/api/people',(req,res)=>{
     const{name}= req.body
     if(!name){
-        return res.status(400).json({success:false, msg:'please provid name value'})
+        return res.status(400).json({sucess:false, msg:'please provid name value'})
     }
     res.status(201).json({success:true, person:name})
 })
