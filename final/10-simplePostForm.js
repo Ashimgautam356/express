d1f653a9743c8a29cdfd8e6eb8e7c6279c129ba1
Ashim Gautam <computer77356@gmail.com>
2023-11-02 21:31:46 +0545

    learned get, post and other stuff's

--- final/10-simplePostForm.js
+++ final/10-simplePostForm.js
@@ -6,20 +6,6 @@ let {people }= require('./data')
 app.use(express.static('./methods-public'))
 //parse form data 
 app.use(express.urlencoded({extended:false}))
-//parse json 
-app.use(express.json())
-
-app.get('/api/people',(req,res)=>{
-    res.status(200).json({sucess:true, data:people})
-})
-
-app.post('/api/people',(req,res)=>{
-    const{name}= req.body
-    if(!name){
-        return res.status(400).json({sucess:false, msg:'please provid name value'})
-    }
-    res.status(201).json({success:true, person:name})
-})
 app.post('/login',(req, res)=>{
     const {name} = req.body
     if(name){
