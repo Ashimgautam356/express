c2e2639db57cec6010802aa98df16e201a0b0e08
Ashim Gautam <computer77356@gmail.com>
2023-11-03 22:00:19 +0545

    learned controllers

--- routes/people.js
+++ routes/people.js
@@ -1,56 +1,21 @@
 const express = require('express');
-let {people }= require('../data')
-
+const {  getPeople,
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletePerson
+} = require('../controllers/people')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.status(200).json({success:true, data:people})
-})
+// router.get('/',getPeople)
+// router.post('/',createPerson)
+// router.post('/postman',createPersonPostman)
+// router.put('/:id',updatePerson)
+// router.delete('/:id',deletePerson)
 
-
-router.post('/',(req, res)=>{
-    const{name} = req.body
-    if(!name){
-        return res.status(400).json({success:false, msg:'please provid name value'})
-    }
-    res.status(201).send({success:true, data:[...people, name]})
-})
-
-router.post('/postman',(req,res)=>{
-    const{name}= req.body
-    if(!name){
-        return res.status(400).json({success:false, msg:'please provid name value'})
-    }
-    res.status(201).json({success:true, person:name})
-})
-
-
-
-router.put('/:id',(req, res)=>{
-    const {id} = req.params
-    const {name} = req.body
-
-    const  person = people.find(person => person.id == Number(id))
-    if(!person){
-        return res.status(400).json({success:false, msg:`no person found with this ${id} id`})
-    }
-    const newPeople = people.map(person=>{
-        if(person.id == Number(id)){
-            person.name = name
-        }
-        return person
-    })
-    res.status(200).json({success: true, data: newPeople})
-})
-
-
-router.delete('/:id',(req,res)=>{
-    const  person = people.find((person) => person.id === Number(req.params.id))
-    if(!person){
-        return res.status(404).json({success:false, msg:`no person found with this ${req.params.id} id`})
-    }
-    const newPeople = people.filter((person)=>  person.id !== Number(req.params.id))
-    return res.status(200).json({success: true, data: newPeople})
-})
+// basically we can chain it but we can do it like above ther is no conventation.
+router.route('/').get(getPeople).post(createPerson)
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(deletePerson)
 
 module.exports = router
